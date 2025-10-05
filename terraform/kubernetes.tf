632f32ec3a2611c62d1e643cec04dc8acc7d6dcd
Gabriel Sahdo <sahdo@motorola.com>
2025-10-05 18:59:08 -0400

    Refactor Kubernetes ConfigMap to use v1_data and enhance IAM role mappings

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -9,16 +9,14 @@ resource "kubernetes_namespace" "tech_challenge" {
   }
 }
 
-# ConfigMap
-resource "kubernetes_config_map" "tech_challenge_config" {
+resource "kubernetes_config_map_v1_data" "aws_auth" {
   metadata {
-    name      = "tech-challenge-config"
+    name      = "tech-challenge"
     namespace = kubernetes_namespace.tech_challenge.metadata[0].name
-    labels = {
-      app         = "tech-challenge"
-      environment = "production"
-    }
   }
+
+  # Força a sobrescrita de quaisquer alterações manuais
+  force = true
 
   data = {
     NODE_ENV                            = "production"
@@ -27,8 +25,57 @@ resource "kubernetes_config_map" "tech_challenge_config" {
     DB_PG_LOGGING                      = "false"
     JWT_ACCESS_TOKEN_EXPIRATION_TIME   = var.jwt_access_token_expiration_time
     JWT_REFRESH_TOKEN_EXPIRATION_TIME  = var.jwt_refresh_token_expiration_time
+    # Mapeia as roles do IAM para grupos do Kubernetes
+    mapRoles = yamlencode([
+      {
+        # Mapeia os nós do EKS para que possam se juntar ao cluster
+        rolearn  = module.eks.worker_iam_role_arn
+        username = "system:node:{{EC2PrivateDNSName}}"
+        groups   = [
+          "system:bootstrappers",
+          "system:nodes",
+        ]
+      },
+      {
+        # IMPORTANTE: Mapeia a sua role do AWS Academy para o grupo de administradores
+        rolearn  = data.aws_caller_identity.current.arn # Usa o ARN da role que está executando o Terraform
+        username = "terraform-admin" # Um nome de usuário para logs
+        groups   = [
+          "system:masters"
+        ]
+      }
+    ])
+    # mapUsers e mapAccounts podem ficar vazios se não forem necessários
+    mapUsers    = yamlencode([])
+    mapAccounts = yamlencode([])
   }
+
+  # Garante que o ConfigMap seja criado/atualizado somente após o cluster estar pronto
+  depends_on = [
+    module.eks.cluster
+  ]
 }
+
+# ConfigMap
+# resource "kubernetes_config_map" "tech_challenge_config" {
+#   metadata {
+#     name      = "tech-challenge-config"
+#     namespace = kubernetes_namespace.tech_challenge.metadata[0].name
+#     labels = {
+#       app         = "tech-challenge"
+#       environment = "production"
+#     }
+#   }
+
+#   data = {
+#     NODE_ENV                            = "production"
+#     DB_PG_PORT                         = var.db_pg_port
+#     DB_PG_NAME                         = var.db_pg_name
+#     DB_PG_LOGGING                      = "false"
+#     JWT_ACCESS_TOKEN_EXPIRATION_TIME   = var.jwt_access_token_expiration_time
+#     JWT_REFRESH_TOKEN_EXPIRATION_TIME  = var.jwt_refresh_token_expiration_time
+#   }
+# }
 
 # Secret
 resource "kubernetes_secret" "tech_challenge_secrets" {
